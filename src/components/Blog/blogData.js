748adb16a19895360bfc68e04949567e3bf65dde
Dantong Li <dtlics2000@gmail.com>
2020-07-12 16:04:47 +0800

    new blog

--- src/components/Blog/blogData.js
+++ src/components/Blog/blogData.js
@@ -1,6 +1,11 @@
 export const blogData = [
-  {
-    content: "Blog system is GO!!!",
-    time: "2020-07-12 14:00",
-  },
-];
+         {
+           content: "Blog system is GO!!!",
+           time: "2020-07-12 14:00",
+         },
+         {
+           content:
+             "I've been studying David Silver's RL course for a week now. But those concepts are really confusing so I started to find some tutorial material to have some exercise. Working on this repo: https://github.com/dalmia/David-Silver-Reinforcement-learning.git, I've created a pull request to fix 2 bugs so that the answer provided by the author can work properly. This PR really feels good, lol.",
+             time: "2020-07-12 16:00"
+         },
+       ];
