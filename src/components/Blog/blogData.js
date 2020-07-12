953d740dff13e6c6a93f09505341191d6b1f8202
Dantong Li <dtlics2000@gmail.com>
2020-07-12 20:34:47 +0800

    Update blogData.js

--- src/components/Blog/blogData.js
+++ src/components/Blog/blogData.js
@@ -8,4 +8,9 @@ export const blogData = [
              "I've been studying David Silver's RL course for a week now. But those concepts are really confusing so I started to find some tutorial material to have some exercise. Working on this repo: https://github.com/dalmia/David-Silver-Reinforcement-learning.git, I've created a pull request to fix 2 bugs so that the answer provided by the author can work properly. This PR really feels good, lol.",
              time: "2020-07-12 16:00"
          },
+         {
+           content: "Just found out there's a huge repo for exercises about RL talored to the David Silver's RL course. Life sucks... Anyway, I'm pretty happy that I did know how to study RL afterall. I tried to look for exercises/tutorials to learn better, and it turns out that I'm right to do so. (Since there a whole repo for this) Cool, let me get to it right away, lol.",
+           time: "2020-07-12 20:34"
+
+         }
        ];
