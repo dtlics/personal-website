e742257f1792166e1073f10baad9b8df33bf6bf4
Dantong Li <dtlics2000@gmail.com>
2020-07-27 17:58:59 +0800

    Update blogData.js

--- src/components/Blog/blogData.js
+++ src/components/Blog/blogData.js
@@ -1,23 +1,30 @@
+import React from "react";
+
 export const blogData = [
-         {
-           content: "Blog system is GO!!!",
-           time: "2020-07-12 14:00",
-         },
-         {
-           content:
-             "I've been studying David Silver's RL course for a week now. But those concepts are really confusing so I started to find some tutorial material to have some exercise. Working on this repo: https://github.com/dalmia/David-Silver-Reinforcement-learning.git, I've created a pull request to fix 2 bugs so that the answer provided by the author can work properly. This PR really feels good, lol.",
-             time: "2020-07-12 16:00"
-         },
-         {
-           content: "Just found out there's a huge repo for exercises about RL talored to the David Silver's RL course. Life sucks... Anyway, I'm pretty happy that I did know how to study RL afterall. I tried to look for exercises/tutorials to learn better, and it turns out that I'm right to do so. (Since there a whole repo for this) Cool, let me get to it right away, lol.",
-           time: "2020-07-12 20:34"
-         },
-         {
-           content: "Ahhh Just quitted on my game design project. It really sucks. Anyway, hopefully the time saved by doing this will finally pay off.",
-           time: "2020-07-15 01:11"
-         },
-         {
-           content: "Year 3 course selection began today, but I just found loads of time clashes among the courses I dream to take. Though heart-broken, there's still hope, lol. So let me write the courses that I'm going to learn by myself down for my future reference. \n MATH3301 Algebra 1\n MATH3401 Analysis 1\n MATH3541 Topology\n MATH3601 Numerical Analysis\n MATH3901 Operation Research\n MATH3904 Optimization\n MATH4302 Algebra 2\n STAT3600 Linear Statistical Analysis\n STAT3603 Stochastic Processes",
-           time: "2020-07-27 17:36"
-         }
-       ];
+  {
+    content: "Blog system is GO!!!",
+    time: "2020-07-12 14:00",
+  },
+  {
+    content:
+      "I've been studying David Silver's RL course for a week now. But those concepts are really confusing so I started to find some tutorial material to have some exercise. Working on this repo: https://github.com/dalmia/David-Silver-Reinforcement-learning.git, I've created a pull request to fix 2 bugs so that the answer provided by the author can work properly. This PR really feels good, lol.",
+    time: "2020-07-12 16:00"
+  },
+  {
+    content: "Just found out there's a huge repo for exercises about RL talored to the David Silver's RL course. Life sucks... Anyway, I'm pretty happy that I did know how to study RL afterall. I tried to look for exercises/tutorials to learn better, and it turns out that I'm right to do so. (Since there a whole repo for this) Cool, let me get to it right away, lol.",
+    time: "2020-07-12 20:34"
+  },
+  {
+    content: "Ahhh Just quitted on my game design project. It really sucks. Anyway, hopefully the time saved by doing this will finally pay off.",
+    time: "2020-07-15 01:11"
+  },
+  {
+    content:
+      <span>
+        Year 3 course selection began today, but I just found loads of time clashes among the courses I dream to take. Though heart-broken, there's still hope, lol. So let me write the courses that I'm going to learn by myself down for my future reference. \n\
+        MATH3301 Algebra 1: Relatively easy, but is very fundamental. What some structures are and their easy propertities.\n \
+        MATH3401 Analysis 1\n MATH3541 Topology\n MATH3601 Numerical Analysis\n MATH3901 Operation Research\n MATH3904 Optimization\n MATH4302 Algebra 2\n STAT3600 Linear Statistical Analysis\n STAT3603 Stochastic Processes
+           </span>,
+    time: "2020-07-27 17:36"
+  }
+];
