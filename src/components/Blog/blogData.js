2b268681fa67c81652884223fc604d056ca616a2
Dantong Li <dtlics2000@gmail.com>
2020-07-27 21:38:57 +0800

    Update blogData.js

--- src/components/Blog/blogData.js
+++ src/components/Blog/blogData.js
@@ -25,39 +25,39 @@ export const blogData = [
         <p><a href="https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=MATH3301"
           target="_blank"
           rel="noopener noreferrer"
-          style={{ color: "black", fontWeight: "bold" }}>MATH3301 Algebra 1: </a>Relatively easy, but is very fundamental. What some structures (group, ring, field) are and their easy propertities.</p>
+          style={{ fontWeight: "bold" }}>MATH3301 Algebra 1: </a>Relatively easy, but is very fundamental. What some structures (group, ring, field) are and their easy propertities.</p>
         <p><a href="https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=MATH4302"
           target="_blank"
           rel="noopener noreferrer"
-          style={{ color: "black", fontWeight: "bold" }}>MATH4302 Algebra 2: </a>More interesting. Part A: What is PID and UFD; Part B: Field Theory, domain extension and Galois Theory; Part C: Module Theory (Based on PID) -- "Best course in HKU"(Ning, 2019)</p>
+          style={{ fontWeight: "bold" }}>MATH4302 Algebra 2: </a>More interesting. Part A: What is PID and UFD; Part B: Field Theory, domain extension and Galois Theory; Part C: Module Theory (Based on PID) -- "Best course in HKU"(Ning, 2019)</p>
         <p><a href="https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=MATH3401"
           target="_blank"
           rel="noopener noreferrer"
-          style={{ color: "black", fontWeight: "bold" }}>MATH3401 Analysis 1: </a>More familiar. Metric Topology. The basis for analytics. A generalization of MATH2241 but 2241 is not a necessity. Useful in understanding CS problems systemetically, especially when dealing with computational geometry stuff.</p>
+          style={{ fontWeight: "bold" }}>MATH3401 Analysis 1: </a>More familiar. Metric Topology. The basis for analytics. A generalization of MATH2241 but 2241 is not a necessity. Useful in understanding CS problems systemetically, especially when dealing with computational geometry stuff.</p>
         <p><a href="https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=MATH3541"
           target="_blank"
           rel="noopener noreferrer"
-          style={{ color: "black", fontWeight: "bold" }}>MATH3541 Topology: </a>Point Set Topology (which can be regarded as a generalization of Metric Topology) with some Algebraic Topology (Including Fundamental Group and Covering Space). Important course and very fundamental. It needs a bit of abstract algebra so it's best to ocnsider it after taking Algebra 1 and Analysis 1. TCS also needs topology, especially if my research interest is computational geometry.</p>
+          style={{ fontWeight: "bold" }}>MATH3541 Topology: </a>Point Set Topology (which can be regarded as a generalization of Metric Topology) with some Algebraic Topology (Including Fundamental Group and Covering Space). Important course and very fundamental. It needs a bit of abstract algebra so it's best to ocnsider it after taking Algebra 1 and Analysis 1. TCS also needs topology, especially if my research interest is computational geometry.</p>
         <p><a href="https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=MATH3901"
           target="_blank"
           rel="noopener noreferrer"
-          style={{ color: "black", fontWeight: "bold" }}>MATH3901 Operations Research 1: </a>Very useful. LP and SDP are very important and are everywhere. "But avoid Xiaoming Yuan since he's course might be a bit awful"(Ning, 2019)</p>
+          style={{ fontWeight: "bold" }}>MATH3901 Operations Research 1: </a>Very useful. LP and SDP are very important and are everywhere. "But avoid Xiaoming Yuan since he's course might be a bit awful"(Ning, 2019)</p>
         <p><a href="https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=MATH3601"
           target="_blank"
           rel="noopener noreferrer"
-          style={{ color: "black", fontWeight: "bold" }}>MATH3601 Numerical Analysis</a></p>
+          style={{ fontWeight: "bold" }}>MATH3601 Numerical Analysis</a></p>
         <p><a href="https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=MATH3904"
           target="_blank"
           rel="noopener noreferrer"
-          style={{ color: "black", fontWeight: "bold" }}>MATH3904 Optimization</a></p>
+          style={{ fontWeight: "bold" }}>MATH3904 Optimization</a></p>
         <p><a href="https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=STAT3600"
           target="_blank"
           rel="noopener noreferrer"
-          style={{ color: "black", fontWeight: "bold" }}>STAT3600 Linear Statistical Analysis</a></p>
+          style={{ fontWeight: "bold" }}>STAT3600 Linear Statistical Analysis</a></p>
         <p><a href="https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=STAT3603"
           target="_blank"
           rel="noopener noreferrer"
-          style={{ color: "black", fontWeight: "bold" }}>STAT3603 Stochastic Processes</a></p>
+          style={{ fontWeight: "bold" }}>STAT3603 Stochastic Processes</a></p>
       </span>,
     time: "2020-07-27 17:36"
   }
