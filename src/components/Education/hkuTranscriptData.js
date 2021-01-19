5bb3b95bd016fcb7d913bd6ac7ea29092c8cc98c
Dantong Li <dtlics2000@gmail.com>
2021-01-19 15:47:05 +0800

    Update hkuTranscriptData.js

--- src/components/Education/hkuTranscriptData.js
+++ src/components/Education/hkuTranscriptData.js
@@ -1,180 +1,234 @@
 export const transcriptData = [
-         {
-           courseCode: "CCST 9017",
-           grade: "A",
-           courseName: "Game Theory",
-           isPostGrad: false,
-           link: "https://commoncore.hku.hk/ccst9017/",
-         },
-         {
-           courseCode: "CUND 9003",
-           grade: "A-",
-           courseName: "Cantonese",
-           isPostGrad: false,
-           link:
-             "http://web.chinese.hku.hk/main/chinese-language-enhancement-program/",
-         },
-         {
-           courseCode: "ENGG 1300",
-           grade: "A-",
-           courseName: "Fundamental Mechanics",
-           isPostGrad: false,
-           link: "",
-         },
-         {
-           courseCode: "ENGG 1320",
-           grade: "B+",
-           courseName: "Entrepreneurship",
-           isPostGrad: false,
-           link: "https://innowing.hk/innoshow1/2019show1-engg1320",
-         },
-         {
-           courseCode: "ENGG 1330",
-           grade: "A+",
-           courseName: "Python",
-           isPostGrad: false,
-           link:
-             "https://www.cs.hku.hk/index.php/programmes/course-offered?infile=2018/engg1330.html",
-         },
-         {
-           courseCode: "MATH 1853",
-           grade: "A+",
-           courseName: "Linear Algebra & Probabilities",
-           isPostGrad: false,
-           link:
-             "https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=MATH1853",
-         },
-         {
-           courseCode: "CAES 1000",
-           grade: "A-",
-           courseName: "English",
-           isPostGrad: false,
-           link: "https://learning.hku.hk/caes1000/",
-         },
-         {
-           courseCode: "CCCH 9001",
-           grade: "A-",
-           courseName: "Chinese Architecture",
-           isPostGrad: false,
-           link: "https://commoncore.hku.hk/ccch9001/",
-         },
-         {
-           courseCode: "ENGG 1310",
-           grade: "A+",
-           courseName: "Electricity and electronics",
-           isPostGrad: false,
-           link: "",
-         },
-         {
-           courseCode: "ENGG 1340",
-           grade: "A+",
-           courseName: "C++ and Programming Tools",
-           isPostGrad: false,
-           link:
-             "https://www.cs.hku.hk/index.php/programmes/course-offered?infile=2018/engg1340.html",
-         },
-         {
-           courseCode: "MATH 1851",
-           grade: "A+",
-           courseName: "Calculus and ODE",
-           isPostGrad: false,
-           link:
-             "https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=MATH1851",
-         },
-         {
-           courseCode: "CCST 9073",
-           grade: "Pass",
-           courseName: "Emotion, Cognition and Brain",
-           isPostGrad: false,
-           link: "https://commoncore.hku.hk/ccst9073/",
-         },
-         {
-           courseCode: "COMP 2121",
-           grade: "A+",
-           courseName: "Discrete Mathematics",
-           isPostGrad: false,
-           link:
-             "https://www.cs.hku.hk/index.php/programmes/course-offered?infile=2019/comp2121.html",
-         },
-         {
-           courseCode: "COMP 2396",
-           grade: "A+",
-           courseName: "JAVA & OOP",
-           isPostGrad: false,
-           link:
-             "https://www.cs.hku.hk/index.php/programmes/course-offered?infile=2019/comp2396.html",
-         },
-         {
-           courseCode: "MATH 2101",
-           grade: "A-",
-           courseName: "Linear Algebra",
-           isPostGrad: false,
-           link:
-             "https://www.cs.hku.hk/index.php/programmes/course-offered?infile=2019/comp2396.html",
-         },
-         {
-           courseCode: "MATH 2211",
-           grade: "A",
-           courseName: "Multivariate Calculus",
-           isPostGrad: false,
-           link:
-             "https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=MATH2211",
-         },
-         {
-           courseCode: "STAT 2601",
-           grade: "A+",
-           courseName: "Probabilities and Statistics 1",
-           isPostGrad: false,
-           link:
-             "https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=STAT2601",
-         },
-         {
-           courseCode: "COMP 2119",
-           grade: "A+",
-           courseName: "Intro to Algorithms & Data Structure",
-           isPostGrad: false,
-           link:
-             "https://www.cs.hku.hk/index.php/programmes/course-offered?infile=2019/comp2119.html",
-         },
-         {
-           courseCode: "COMP 2120",
-           grade: "A+",
-           courseName: "Computer Organization",
-           isPostGrad: false,
-           link:
-             "https://www.cs.hku.hk/index.php/programmes/course-offered?infile=2019/comp2120.html",
-         },
-         {
-           courseCode: "COMP 3250",
-           grade: "A",
-           courseName: "Advanced Design & Analysis of Algorithms",
-           isPostGrad: false,
-           link:
-             "https://www.cs.hku.hk/index.php/programmes/course-offered?infile=2019/comp3250.html",
-         },
-         {
-           courseCode: "COMP 7309",
-           grade: "A-",
-           courseName: "Quantum Computing and AI",
-           isPostGrad: true,
-           link: "https://msccs.cs.hku.hk/public/courses/2019/COMP7309A/",
-         },
-         {
-           courseCode: "MATH 2241",
-           grade: "A+",
-           courseName: "Intro to Mathematical Analysis",
-           isPostGrad: false,
-           link:
-             "https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=MATH2241",
-         },
-         {
-           courseCode: "STAT 2602",
-           grade: "A+",
-           courseName: "Probabilities and Statistics 2",
-           isPostGrad: false,
-           link:
-             "https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=STAT2602",
-         },
-       ];
+  {
+    courseCode: "CCST 9017",
+    grade: "A",
+    courseName: "Game Theory",
+    isPostGrad: false,
+    link: "https://commoncore.hku.hk/ccst9017/",
+  },
+  {
+    courseCode: "CUND 9003",
+    grade: "A-",
+    courseName: "Cantonese",
+    isPostGrad: false,
+    link:
+      "http://web.chinese.hku.hk/main/chinese-language-enhancement-program/",
+  },
+  {
+    courseCode: "ENGG 1300",
+    grade: "A-",
+    courseName: "Fundamental Mechanics",
+    isPostGrad: false,
+    link: "",
+  },
+  {
+    courseCode: "ENGG 1320",
+    grade: "B+",
+    courseName: "Entrepreneurship",
+    isPostGrad: false,
+    link: "https://innowing.hk/innoshow1/2019show1-engg1320",
+  },
+  {
+    courseCode: "ENGG 1330",
+    grade: "A+",
+    courseName: "Python",
+    isPostGrad: false,
+    link:
+      "https://www.cs.hku.hk/index.php/programmes/course-offered?infile=2018/engg1330.html",
+  },
+  {
+    courseCode: "MATH 1853",
+    grade: "A+",
+    courseName: "Linear Algebra & Probabilities",
+    isPostGrad: false,
+    link:
+      "https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=MATH1853",
+  },
+  {
+    courseCode: "CAES 1000",
+    grade: "A-",
+    courseName: "English",
+    isPostGrad: false,
+    link: "https://learning.hku.hk/caes1000/",
+  },
+  {
+    courseCode: "CCCH 9001",
+    grade: "A-",
+    courseName: "Chinese Architecture",
+    isPostGrad: false,
+    link: "https://commoncore.hku.hk/ccch9001/",
+  },
+  {
+    courseCode: "ENGG 1310",
+    grade: "A+",
+    courseName: "Electricity and electronics",
+    isPostGrad: false,
+    link: "",
+  },
+  {
+    courseCode: "ENGG 1340",
+    grade: "A+",
+    courseName: "C++ and Programming Tools",
+    isPostGrad: false,
+    link:
+      "https://www.cs.hku.hk/index.php/programmes/course-offered?infile=2018/engg1340.html",
+  },
+  {
+    courseCode: "MATH 1851",
+    grade: "A+",
+    courseName: "Calculus and ODE",
+    isPostGrad: false,
+    link:
+      "https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=MATH1851",
+  },
+  {
+    courseCode: "CCST 9073",
+    grade: "Pass",
+    courseName: "Emotion, Cognition and Brain",
+    isPostGrad: false,
+    link: "https://commoncore.hku.hk/ccst9073/",
+  },
+  {
+    courseCode: "COMP 2121",
+    grade: "A+",
+    courseName: "Discrete Mathematics",
+    isPostGrad: false,
+    link:
+      "https://www.cs.hku.hk/index.php/programmes/course-offered?infile=2019/comp2121.html",
+  },
+  {
+    courseCode: "COMP 2396",
+    grade: "A+",
+    courseName: "JAVA & OOP",
+    isPostGrad: false,
+    link:
+      "https://www.cs.hku.hk/index.php/programmes/course-offered?infile=2019/comp2396.html",
+  },
+  {
+    courseCode: "MATH 2101",
+    grade: "A-",
+    courseName: "Linear Algebra",
+    isPostGrad: false,
+    link:
+      "https://www.cs.hku.hk/index.php/programmes/course-offered?infile=2019/comp2396.html",
+  },
+  {
+    courseCode: "MATH 2211",
+    grade: "A",
+    courseName: "Multivariate Calculus",
+    isPostGrad: false,
+    link:
+      "https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=MATH2211",
+  },
+  {
+    courseCode: "STAT 2601",
+    grade: "A+",
+    courseName: "Probabilities and Statistics 1",
+    isPostGrad: false,
+    link:
+      "https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=STAT2601",
+  },
+  {
+    courseCode: "COMP 2119",
+    grade: "A+",
+    courseName: "Intro to Algorithms & Data Structure",
+    isPostGrad: false,
+    link:
+      "https://www.cs.hku.hk/index.php/programmes/course-offered?infile=2019/comp2119.html",
+  },
+  {
+    courseCode: "COMP 2120",
+    grade: "A+",
+    courseName: "Computer Organization",
+    isPostGrad: false,
+    link:
+      "https://www.cs.hku.hk/index.php/programmes/course-offered?infile=2019/comp2120.html",
+  },
+  {
+    courseCode: "COMP 3250",
+    grade: "A",
+    courseName: "Advanced Design & Analysis of Algorithms",
+    isPostGrad: false,
+    link:
+      "https://www.cs.hku.hk/index.php/programmes/course-offered?infile=2019/comp3250.html",
+  },
+  {
+    courseCode: "COMP 7309",
+    grade: "A-",
+    courseName: "Quantum Computing and AI",
+    isPostGrad: true,
+    link: "https://msccs.cs.hku.hk/public/courses/2019/COMP7309A/",
+  },
+  {
+    courseCode: "MATH 2241",
+    grade: "A+",
+    courseName: "Intro to Mathematical Analysis",
+    isPostGrad: false,
+    link:
+      "https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=MATH2241",
+  },
+  {
+    courseCode: "STAT 2602",
+    grade: "A+",
+    courseName: "Probabilities and Statistics 2",
+    isPostGrad: false,
+    link:
+      "https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=STAT2602",
+  },
+  {
+    courseCode: "CCGL 9021",
+    grade: "Pass",
+    courseName: "Globalization and Tourism",
+    isPostGrad: false,
+    link: "https://commoncore.hku.hk/ccgl9021/",
+  },
+  {
+    courseCode: "CCHU 9039",
+    grade: "Pass",
+    courseName: "Sexuality and Culture",
+    isPostGrad: false,
+    link: "https://commoncore.hku.hk/cchu9039/",
+  },
+  {
+    courseCode: "COMP 3230",
+    grade: "A+",
+    courseName: "Principles of Operating Systems",
+    isPostGrad: false,
+    link:
+      "https://www.cs.hku.hk/index.php/programmes/course-offered?infile=2020/comp3230.html",
+  },
+  {
+    courseCode: "COMP 3297",
+    grade: "A+",
+    courseName: "Software Engineering",
+    isPostGrad: false,
+    link:
+      "https://www.cs.hku.hk/index.php/programmes/course-offered?infile=2020/comp3297.html",
+  },
+  {
+    courseCode: "COMP 3340",
+    grade: "A+",
+    courseName: "Applied Deep Learning",
+    isPostGrad: false,
+    link:
+      "https://www.cs.hku.hk/index.php/programmes/course-offered?infile=2020/comp3340.html",
+  },
+  {
+    courseCode: "STAT 7609",
+    grade: "C+",
+    courseName: "Research Methods in Statistics",
+    isPostGrad: true,
+    link:
+      "https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=STAT7609",
+  },
+  {
+    courseCode: "STAT 7611",
+    grade: "A",
+    courseName: "Computational Statistics",
+    isPostGrad: true,
+    link:
+      "https://webapp.science.hku.hk/sr4/servlet/enquiry?Type=Course&course_code=STAT7611",
+  },
+];
 
-export const cGPA = "4.05";
+export const cGPA = "4.01";
